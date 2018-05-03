b8c23dd3322fc668b1246af971a46e6f07e5d77b
Brian Lynch <bglynch17@gmail.com>
2018-05-03 16:12:10 +0000

    Weather API working and Example Readme

---
+++ README.md
@@ -0,0 +1,44 @@
+# Hastag Indicates Heading H1
+## Hastag Indicates Heading H2
+### Hastag Indicates Heading H3
+#### Hastag Indicates Heading H4
+##### Hastag Indicates Heading H5
+
+Normal text Normal text Normal text *Italic text* Normal text **Bold text** Normal text ***Italic and Bold text*** Normal text 
+- Bullet point
+- Bullet point
+- Bullet point
+- Bullet point
+
+* Bullrt point
+* Bullrt point
+* Bullrt point
+* Bullrt point
+    * hfbdj
+    * snjsndj
+    * dn snjsndj
+
+1. text
+2. jdsbj
+3. sjnjd
+    1. dhjsbd
+    2. dnsjkdn
+    3. ksnmdk
+        1. djsndj
+        2. dksnkd
+
+
+[This is a link to the outside world](http://www.google.com)
+
+This game is a hosteds <a href="link" target="blank">On Github Pages Her</a>
+
+```javascript
+let a = "Hello World";
+```
+```html
+<a href="" target="blank">ghvsdhsv</a>
+```
+
+jbdjsbdjbs djsbdjbsjd
+
+jnsbdjbs djbsjd
--- Weather API/api.js
+++ Weather API/api.js
@@ -2,28 +2,37 @@ let request = new XMLHttpRequest();
 
 function displayNicely(apiData){
     let newData = JSON.parse(apiData);
-    console.log(newData);
     let htmlString = "<div class='text1'>City:"+"<span>"+newData.name+"</span></div>";
+    console.log(newData);
     htmlString += "<div class='text1'>Current Weather:"+"<span>"+newData.weather[0].description+"</span></div>";
     document.getElementById("weather").innerHTML = htmlString;
-    document.getElementById("icon").innerHTML = "<img src='http://openweathermap.org/img/w/"+newData.weather[0].icon+"10n.png'>";
+    document.getElementById("icon").src = "https://openweathermap.org/img/w/" + newData.weather[0].icon + ".png";
+    
 }
 
 request.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){ 
         displayNicely(this.responseText);     
     }else if(this.readyState == 4 && this.status == 404){
-        document.getElementById("weather").innerHTML="<strong>City Not Found. Please Try Again!</strong>"
+        document.getElementById("weather").innerHTML="<strong>City Not Found. Please Try Again!</strong>";
     } 
 };
 
-let appKey = "6ebea87dfc131fd5402906ce4b098ab8";
+let apiKey = "4ad3fa3ae062c63dbdc440265be60625";
 
 function myCity(){
     let cityName = document.getElementById("uniqueID").value;
-    request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q="+myCity.cityName+"&APPID="); 
+    request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&APPID="+apiKey); 
 
-    request.send(); 
+/*Matts Solution 
+function submitCity(){
+    city = document.getElementById("cityForm")["city"].value;
+    request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKEY);
+}*/
+    
+    
+    request.send();
+    return false;
 }
 
 
